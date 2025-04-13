0df4eaa6dff58613266458bb85764d25f9339e9b
AvanthiAnand24 <aanan43@uic.edu>
2025-04-13 16:14:32 -0500

    commented the save from generate pop

--- pipeline_testing/pipeline/generate_pop.py
+++ pipeline_testing/pipeline/generate_pop.py
@@ -21,8 +21,9 @@ def compute_popularity_model(movie_df, top_n=20):
     return top_movies["movie_id"].head(top_n).tolist()
 
 def save_popularity_model(popular_ids, path="models/popular_movies.pkl"):
-    with open(path, "wb") as f:
-        pickle.dump(popular_ids, f)
+    # with open(path, "wb") as f:
+    #     pickle.dump(popular_ids, f)
+    print ("commented save- testing")
 
 def train_and_save_popularity(movie_df, top_n=20):
     popular_ids = compute_popularity_model(movie_df, top_n)
