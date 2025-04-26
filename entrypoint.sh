d70010af404eb25c790563cf36a128fef4abcf66
sanjith314 <sanjithjaishankar@gmail.com>
2025-04-26 17:48:50 -0500

    Change

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Start mlflow server in the background
-mlflow server \
-     --backend-store-uri sqlite:///mlflow.db \
-     --default-artifact-root /app/mlruns \
-     --host 127.0.0.1 \
-     --port 6001 &
+# mlflow server \
+#      --backend-store-uri sqlite:///mlflow.db \
+#      --default-artifact-root /app/mlruns \
+#      --host 127.0.0.1 \
+#      --port 6001 &
 
 # Wait a bit to make sure mlflow has started (optional but good practice)
-sleep 5
+# sleep 5
 
 # Start the training pipeline
 python pipeline/training_pipeline.py
