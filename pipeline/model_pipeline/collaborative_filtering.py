06a1163d09f9948b510618956cc524319f294624
sanjith314 <sanjithjaishankar@gmail.com>
2025-04-27 15:01:56 -0500

    Change to log model

--- pipeline/model_pipeline/collaborative_filtering.py
+++ pipeline/model_pipeline/collaborative_filtering.py
@@ -224,7 +224,7 @@ class CollaborativeFiltering:
                 np.save("model_artifacts/item_factors.npy", model_info["item_factors"])
 
                 # Log artifacts
-                mlflow.log_artifacts("model_artifacts")
+                mlflow.log_artifacts("models/cf_model.pkl", artifact_path="model")
             
                 print("Model logged to MLflow successfully.")
 
